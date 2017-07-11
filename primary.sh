fe12e5482d1d70ea2aeed5df4d7a932d6bb54a92
Gerard Hoeksema <gerard_hoeksema@live.nl>
2017-07-11 17:24:18 +0200

    Add newest version at primary and secondary

--- primary.sh
+++ primary.sh
@@ -11,8 +11,19 @@ sudo sed -i.bak '/127.0.0.1 localhost/ i\127.0.0.1 $configuredHostname' /etc/hos
 ipAddress="$(ip -4 -o address | grep 10.8 | awk '{print $4}' | cut -d/ -f1)"
 echo $ipAddress>/home/ubuntu/localip.txt
 
-# Up-to-date brengen van het systeem
-sudo apt update && upgrade -y
+# Up-to-date brengen van het systeem & installeren curl voor later gebruik bij Docker bootstrap script.
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install curl -y
 
 # Installeren van Salt Minion pakketten
 sudo apt install salt-minion -y
+
+# Clonen van de GIT repository en overzetten minion configuratie
+sudo git clone https://github.com/HammerheadNL/saltstack.git
+sudo cp -f /home/ubuntu/saltstack/minionconfig/minion /etc/salt
+
+# Het secundaire set-up script uitvoerbaar maken
+sudo chmod +x /home/ubuntu/saltstack/secondary.sh
+
+# Starten van de salt-minion daemon
+sudo salt-minion -d
--- secondary.sh
+++ secondary.sh
@@ -3,4 +3,34 @@
 # primary.sh
 # Secundaire set-up Salt Minion
 
+# Alvast klaarzetten van het IP adres van de minion voor later gebruik
+ipAddress="$(ip -4 -o address | grep 10.8 | awk '{print $4}' | cut -d/ -f1)"
+
+# Updaten van de repositories en het minion systeem
+sudo apt update -y && sudo apt upgrade -y
+
+# Installeren van Nagios NRPE
+sudo apt install nagios-nrpe-server -y
+
+# Installeren van Docker middels installatiescript van Docker zelf.
+sudo curl -fsSL https://get.docker.com/ | sh
+sudo service docker restart
+
+# Het aanmaken van een plaats voor de Wordpress container
+mkdir ~/wordpress && cd ~/wordpress
+
+# Downloaden van Maria Image en pre-configuratie van MariaDB
+sudo docker run -e MYSQL_ROOT_PASSWORD=WordpressDB -e MYSQL_DATABASE=wordpressdb --name mariadb -v "$PWD/database":/var/lib/mysql -d mariadb:latest
+
+# Starten van het Maria database systeem.
+sudo docker start mariadb
+
+# Downloaden Wordpress Image bij Docker
+sudo docker pull wordpress
+
+# Aanmaken content delivery map voor de Docker Wordpress installatie & installeren Wordpress en koppeling naar MariaDB.
+sudo mkdir -p /var/www/html
+cd /var/www/html
+sudo docker run -e WORDPRESS_DB_PASSWORD=WordpressDB --name wordpress --link mariadb:mysql -p $ipAddress:80:80 -v "$PWD":/var/www/html -d wordpress
+
 
